27a54dba3e235b2dc5df11802a9c93faa7473b56
lseaf <ly_sea_af@sina.cn>
2020-11-08 18:41:00 +0800

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from "@angular/router";
+import { AuthService } from './core/auth.service';
+import { TodoComponent } from './todo/todo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path : 'login',
+        component : LoginComponent
+      },
+      {
+        path : 'todo',
+        component : TodoComponent
+      },
+      {
+        path : '',
+        redirectTo : 'todo',
+        pathMatch : 'full'
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    { 
+      provide : 'auth',
+      useClass : AuthService
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
